93d77f5dd32aa939749d06f23f87e605d53dcdb9
amitgr4 <amitgr4@gmail.com>
2024-01-05 16:00:31 +0530

    initial commmit

---
+++ HomeLoan/src/learning/Loan.java
@@ -0,0 +1,20 @@
+package learning;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Loan {
+
+	public static void main(String[] args) {
+		
+
+	    Date date = new Date();  
+	    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");  
+	    String strDate= formatter.format(date);  
+	    System.out.println(strDate);  
+	    System.out.println("this is java project learn git loan");
+	    System.out.println("This is jenkins for loans project");
+
+	}
+
+}
